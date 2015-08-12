9bb5da5cb4e6ca71c34cff76315fef20469470ab
Vishvesh Shah <vmshah@scu.edu>
2015-08-11 19:51:20 -0700

    Init

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+var cfenv = require('cfenv');
 var http = require('http');
+
+var appEnv = cfenv.getAppEnv();
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(appEnv.port, appEnv.bind);
-console.log('Server running');
+console.log('Server running' + appEnv.url);
